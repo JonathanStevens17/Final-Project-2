7a521574c39397869ba1441bebc53286f3e04bc4
JonathanStevens17 <7JStevens@stpatrick.org>
2016-12-12 10:53:42 -0600

    day 1 finished item

---
+++ First-Semester-Final-master/cart.js
@@ -0,0 +1,14 @@
+class Cart{
+  //What is the first part of every class? Type it below
+constructor(name, price, shipping){
+  this.name = n;
+  this.price = p;
+  this.shipping = s;
+  
+}
+
+
+  //Type the instance functions below this comment.
+
+
+}
