e0db91af56234a7ca3c6ca0734fe24d041d47d61
JonathanStevens17 <7JStevens@stpatrick.org>
2016-12-13 10:48:47 -0600

    Finished Item and Cart

---
+++ Final-Project-2-master/First-Semester-Final-master/password.js
@@ -0,0 +1,17 @@
+class Password{
+  //The first part of the class.
+constructor(pk, pvk){
+  this.publickey = pk
+  this.privatekey = pvk
+}
+
+
+  //Instance functions below this comment.
+validpublickey(){
+
+  
+}
+
+
+  //Static function below this comment.
+}
