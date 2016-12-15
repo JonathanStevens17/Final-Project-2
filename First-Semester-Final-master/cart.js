8a38cee1a6ef88ccda8fa18fb935dd3e24d435b8
JonathanStevens17 <7JStevens@stpatrick.org>
2016-12-15 10:51:13 -0600

    WIP

--- First-Semester-Final-master/cart.js
+++ First-Semester-Final-master/cart.js
@@ -1,14 +1,30 @@
 class Cart{
   //What is the first part of every class? Type it below
-constructor(name, price, shipping){
-  this.name = n;
-  this.price = p;
-  this.shipping = s;
-  
+constructor(itemList, itemQuantity){
+ this.itemList = [];
+ this.itemQuantity = [];
 }
 
 
   //Type the instance functions below this comment.
+addItem(i, q){
+this.itemList.push(i);
+this.itemQuantity.push(q);
+}
 
-
+totalCart(){
+  let total = 0
+  for(var c=0;c < this.itemList[].length;c++)
+   total = total+this.itemList[c].price+this.itemQuantity[c];
+}
+ return total;
+}
+subCart(days){
+ let subCart = new Cart();
+  for(var c=0;c < this.itemList[].length;a++)
+    if(this.itemList[c].shipping == days){
+      subCart.itemList.push(this.itemList[c]);
+      subCart.itemQuantity.push(this.itemQuantity[c]);
+    }
+return subCart;
 }
--- First-Semester-Final-master/password.js
+++ First-Semester-Final-master/password.js
@@ -1,11 +1,12 @@
 class Password{
   //The first part of the class.
-
+constructor (publickey, privatekey){
+  this.publickey = pk
+  this.privatekey = p
+}
 
 
   //Instance functions below this comment.
 
 
-
   //Static function below this comment.
-}
